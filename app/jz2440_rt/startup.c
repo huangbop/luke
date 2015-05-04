0bab6869cb5e19c46c2634c12ad187dc608ae445
Huang Bo <huangbop@gmail.com>
2015-05-04 15:54:40 +0800

    Key interrupt test OK, with mmu

--- app/jz2440_rt/startup.c
+++ app/jz2440_rt/startup.c
@@ -4,19 +4,23 @@
 
 int main()
 {
-	rt_hw_interrupt_disable();
+	rt_hw_mmu_init();
+
 	
-	board_init();
 
-	rt_show_version();
+/* rt_hw_interrupt_disable(); */
+	
+	/* board_init(); */
 
-	rt_system_scheduler_init();
+	/* rt_show_version(); */
 
-	application_init();
+	/* rt_system_scheduler_init(); */
 
-	rt_thread_idle_init();
+	/* application_init(); */
 
-	rt_system_scheduler_start();
+	/* rt_thread_idle_init(); */
+
+	/* rt_system_scheduler_start(); */
 
 	return 0;
 }
