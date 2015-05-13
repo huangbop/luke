15f8ca1a72816d61ec9aeabf567994a2c3907e86
Huang Bo <huangbop@gmail.com>
2015-05-13 19:26:26 +0800

    Alternatively print OK

--- app/hynix_rt/application.c
+++ app/hynix_rt/application.c
@@ -1,6 +1,39 @@
+#include "rtthread.h"
 
 
-void application_init()
+static struct rt_thread print0;
+static unsigned char print0_stack[512];
+
+static struct rt_thread print1;
+static unsigned char print1_stack[512];
+
+
+void print0_entry(void)
 {
+	while (1) {
+		rt_kprintf("0 0 0 0 \n");
 
+	}
+}
+
+void print1_entry(void)
+{
+	while (1) {
+		rt_kprintf("1 1 1 1 \n");
+
+	}
+}
+
+void application_init(void)
+{
+	int res;
+
+	res = rt_thread_init(&print0, "print0", print0_entry, 0,
+			     print0_stack, 512, 8, 3);
+
+	res = rt_thread_init(&print1, "print1", print1_entry, 0,
+			     print1_stack, 512, 8, 3);
+
+	rt_thread_startup(&print0);
+	rt_thread_startup(&print1);
 }
