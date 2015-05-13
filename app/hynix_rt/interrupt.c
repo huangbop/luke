6a66773d922bb8d821f28a28441c5f4f0001dc5e
Huang Bo <huangbop@gmail.com>
2015-05-13 17:18:17 +0800

    Get timer0 interrupt

--- app/hynix_rt/interrupt.c
+++ app/hynix_rt/interrupt.c
@@ -1,5 +1,20 @@
+#include "mSFRS.h"
 
 
 unsigned interrupt_switch_flag;
 unsigned interrupt_switch_from;
 unsigned interrupt_switch_to;
+
+
+void do_irq(void)
+{
+	unsigned short peri_int_sts;
+
+
+	peri_int_sts = rCPU.periIntrStatus.all;
+
+	if (peri_int_sts & 0x0100) { /* timer0 interrupt */
+		rt_tick_increase();
+	}
+
+}
